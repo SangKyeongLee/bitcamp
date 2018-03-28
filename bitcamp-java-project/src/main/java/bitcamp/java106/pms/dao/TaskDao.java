f93c95a3f21816d52743853c7c5e67126f390198
sangkyeonglee <sangkyeong.lee93@gmail.com>
2018-03-28 20:03:52 +0900

    collection

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/TaskDao.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/TaskDao.java
@@ -1,35 +1,74 @@
 package bitcamp.java106.pms.dao;
 
 import bitcamp.java106.pms.domain.Task;
+import bitcamp.java106.pms.util.ArrayList;
 
 public class TaskDao {
-    Task[] tasks = new Task[1000];
-    int taskIndex = 0;
+    private ArrayList collection = new ArrayList();
     
     public void insert(Task task) {
-        task.setNo(taskIndex);
-        this.tasks[this.taskIndex++] = task;
+        this.collection.add(task);
     }
     
-    public Task[] list() {
-        Task[] arr = new Task[taskIndex];
-        for (int i = 0; i < taskIndex; i++) 
-            arr[i] = tasks[i];
+    private int count(String teamName) {
+        int cnt = 0;
+        for (int i = 0; i < this.collection.size(); i++) {
+            Task task = (Task)collection.get(i);
+            if (task.getTeam().getName().toLowerCase().equals(
+                    teamName.toLowerCase())) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    public Task[] list(String teamName) {
+        Task[] arr = new Task[this.count(teamName)];
+        for (int i = 0, x = 0; i < collection.size(); i++) {
+            Task task = (Task)collection.get(i);
+            if (task.getTeam().getName().toLowerCase().equals(
+                    teamName.toLowerCase())) {
+                arr[x++] = task;
+            }
+        }
         return arr;
     }
     
-    public Task get(int i) {
-        if (i < 0 || i >= taskIndex)
+    public Task get(int taskNo) {
+        int index = this.getTaskIndex(taskNo);
+        if (index < 0)
             return null;
-        return tasks[i];
+        return (Task)collection.get(index);
     }
     
     public void update(Task task) {
-        tasks[task.getNo()] = task;
+        int index = this.getTaskIndex(task.getNo());
+        if (index < 0)
+            return;
+        this.collection.set(index, task);
     }
     
-    public void delete(int i) {
-        tasks[i] = null;
+    public void delete(int taskNo) {
+        int index = this.getTaskIndex(taskNo);
+        if (index < 0)
+            return;
+        this.collection.remove(index);
+    }
+    
+    private int getTaskIndex(int taskNo) {
+        for (int i = 0; i < collection.size(); i++) {
+            Task task = (Task) collection.get(i);
+            if (task.getNo() == taskNo) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
+
+// ver 17 - 클래스 생성
+
+
+
+
 
--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/TeamDao.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/TeamDao.java
@@ -1,46 +1,48 @@
 package bitcamp.java106.pms.dao;
 
 import bitcamp.java106.pms.domain.Team;
+import bitcamp.java106.pms.util.ArrayList;
 
 public class TeamDao {
-    Team[] teams = new Team[1000];
-    int teamIndex = 0;
+    private ArrayList collection = new ArrayList();
     
     public void insert(Team team) {
         // 팀 정보가 담겨있는 객체의 주소를 배열에 보관한다.
-        this.teams[this.teamIndex++] = team;
+        this.collection.add(team);
     }
     
     public Team[] list() {
-        Team[] arr = new Team[this.teamIndex];
-        for (int i = 0; i < this.teamIndex; i++) 
-            arr[i] = this.teams[i];
+        Team[] arr = new Team[this.collection.size()];
+        for (int i = 0; i < this.collection.size(); i++) 
+            arr[i] = (Team)this.collection.get(i);
         return arr;
     }
     
     public Team get(String name) {
-        int i = this.getTeamIndex(name);
-        if (i == -1)
+        int index = this.getTeamIndex(name);
+        if (index < 0)
             return null;
-        return teams[i];
+        return (Team)this.collection.get(index);
     }
     
     public void update(Team team) {
-        int i = this.getTeamIndex(team.getName());
-        if (i != -1)
-            teams[i] = team;
+        int index = this.getTeamIndex(team.getName());
+        if (index < 0)
+            return;
+        this.collection.set(index, team);
     }
     
     public void delete(String name) {
-        int i = this.getTeamIndex(name);
-        if (i != -1) 
-            teams[i] = null;
+        int index = this.getTeamIndex(name);
+        if (index < 0)
+            return;
+        this.collection.remove(index);
     }
     
     private int getTeamIndex(String name) {
-        for (int i = 0; i < this.teamIndex; i++) {
-            if (this.teams[i] == null) continue;
-            if (name.equals(this.teams[i].getName().toLowerCase())) {
+        for (int i = 0; i < this.collection.size(); i++) {
+            Team team = (Team)this.collection.get(i);
+            if (name.equals(team.getName().toLowerCase())) {
                 return i;
             }
         }

eaf947b515805326bc069c3ae10e783276773287
sangkyeonglee <sangkyeong.lee93@gmail.com>
2018-06-08 16:54:43 +0900

    AOP 필터

---
+++ spring-ioc/src/main/java/bitcamp/java106/step13/ex03/X.java
@@ -0,0 +1,13 @@
+package bitcamp.java106.step13.ex03;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class X {
+    public int m1(int a, int b) {
+        System.out.println("X.m1()");
+        int result = a / b;
+        
+        return result;
+    }
+}
